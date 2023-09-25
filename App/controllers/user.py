6fa0c240a1fdb207f02ff332f648805473170e46
Devesh Motie <97921518+D-e-v-M@users.noreply.github.com>
2023-09-25 16:51:34 -0400

    Modified functions

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,14 +1,17 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
+def create_user(email, name, password):
+    newuser = User(email=email,name=name, password=password)
     db.session.add(newuser)
     db.session.commit()
     return newuser
 
-def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
+def get_user_by_email(email):
+    return User.query.filter_by(email=email).first()
+
+def get_user_by_name(name):
+    return User.query.filter_by(name=name).first()
 
 def get_user(id):
     return User.query.get(id)
