66ed327bf2d934b60b8f1af1f75ef31f4b160877
Devesh Motie <97921518+D-e-v-M@users.noreply.github.com>
2023-09-25 19:05:19 -0400

    Yet another fix for review

--- App/models/review.py
+++ App/models/review.py
@@ -5,16 +5,19 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     review = db.Column(db.String, nullable = False)
     student_id = db.Column(db.Integer, db.ForeignKey(Student.id), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
 
     student = db.relationship(Student, backref='students')
 
     def __init__(self, review, student_id):
         self.review = review
-        self.student_id=student_id
+        self.student_id = student_id
+        self.score = 0
 
     def get_json(self):
         return{
             'id': self.id,
-            'name': self.name,
-            'karma': self.karma
+            'review' : self.review,
+            'student_id' : self.student_id,
+            'score' : self.score
         }
