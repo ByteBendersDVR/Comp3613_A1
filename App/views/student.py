86ecd91f3c5a9253ba8c7c345e7f59a08cc2b048
Rich <97922319+RichardR963@users.noreply.github.com>
2023-09-25 22:00:55 -0400

    bruh, a fullstop instead of a comma

--- App/views/student.py
+++ App/views/student.py
@@ -29,7 +29,7 @@ def getParticularStudent(id):
     return jsonify(message=(f'Student with id:{id} could not be found.')), 401
 
 # this adds a new student, user must be logged in 
-@student_views.route('/addstudent'. methods=["POST"])
+@student_views.route('/addstudent', methods=["POST"])
 @jwt_required()
 def addStudent():
     data = request.json
